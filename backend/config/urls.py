b0d27591a0b9b2258b310249d867684cbb42963a
Fernando <julio8martins@gmail.com>
2026-05-04 04:12:03 -0300

    fixes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def root_redirect(_request):
+    return redirect('/api/auth/login/')
 
 urlpatterns = [
+    path('', root_redirect),
     path('api/auth/', include('apps.accounts.urls')),
 ]
